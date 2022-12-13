ec739f6714aa3950b1341f25314962209dac76f6
EliRalli <elikrallison@gmail.com>
2022-12-13 09:10:04 -0700

    adding day3

---
+++ Day3/AdventExample.php
@@ -0,0 +1,12 @@
+<?php
+$examplecode=[
+'vJrwpWtwJgWrhcsFMMfFFhFp',
+'jqHRNqRjqzjGDLGLrsFMfFZSrLrFZsSL',
+'PmmdzqPrVvPwwTWBwg',
+'wMqvLMZHhHMvwLHjbvcjnnSBnvTQFn',
+'ttgJtRGJQctTZtZT',
+'CrZsJsPPZsGzwwsLwLmpwMDw'
+];
+
+
+?>
