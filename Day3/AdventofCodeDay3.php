5137976a7b5e0664c6cdce45c70083d74d372e7c
EliRalli <elikrallison@gmail.com>
2022-12-15 15:04:57 -0700

    almost there

--- Day3/AdventofCodeDay3.php
+++ Day3/AdventofCodeDay3.php
@@ -5,22 +5,23 @@ $examplecode = file_get_contents("D:\AdventCode2022\Day3\AdventExample.txt");
 $lines=explode("\n", $examplecode);
 
 //find length of each element
-$ascnum = mb_chr($examplecode);
-if ($ascnum > 64 && $ascnum < 91){
+// $ascnum = ord($lines);
+// if ($ascnum > 64 && $ascnum < 91){
 
-    return $ascnum -38; //should give the Capital letters
-}
-elseif ( $ascnum > 96 && $ascnum < 123){
+//     return $ascnum -38; //should give the Capital letters
+// }
+// elseif ( $ascnum > 96 && $ascnum < 123){
 
-    return $ascnum- 96; // Gives the score values based off the asc2 numbers for lowercase 1-26
-}
-
+//     return $ascnum- 96; // Gives the score values based off the asc2 numbers for lowercase 1-26
+// }
+//print_r($ascnum);
 $score=0;
 $array=[];
 
 foreach($lines as $key => $line){
     
     if($line == ""){
+
         unset($lines[$key]);
     }
     else{
@@ -29,12 +30,12 @@ foreach($lines as $key => $line){
         $section1=str_split(substr($line,0,$count/2));
         $section2=str_split(substr($line,$count/2,$count));
         $Items= array_unique(array_intersect($section1,$section2));
-        $Items= array_map(function($item) use ($ascnum){
-            return $ascnum[$item];
-        },$Items);
+        
         foreach($Items as $key => $Item){
 
             $array[]=$Item;
+            print_r($array);
+            
         }
 
 
@@ -46,27 +47,29 @@ foreach($lines as $key => $line){
 
 
 //split it halfway
-function ElementSplitter($examplecode): array{
-    $SplitElements=array_chunk($examplecode,count($examplecode)/2);
-    echo $SplitElements;
-    return $SplitElements;
+// function ElementSplitter($examplecode): array{
 
-}
-//find the common letter
+//     $SplitElements=array_chunk($examplecode,count($examplecode)/2);
+//     echo $SplitElements;
+//     return $SplitElements;
+
+// }
+// //find the common letter
 
 
-//assign score to the letter based on the key..... don't make array for each number yet... ASCII?
-function GetValues() {
-    $ascnum = mb_ord($SplitElements);
-    if ($ascnum > 64 && $ascnum < 91){
-        return $ascnum -38; //should give the Capital letters
-    }
-    elseif ( $ascnum > 96 && $ascnum < 123){
-        return $ascnum- 96; // Gives the score values based off the asc2 numbers for lowercase 1-26
-    }
+// //assign score to the letter based on the key..... don't make array for each number yet... ASCII?
+// function GetValues() {
+
+//     $ascnum = mb_ord($SplitElements);
+//     if ($ascnum > 64 && $ascnum < 91){
+//         return $ascnum -38; //should give the Capital letters
+//     }
+//     elseif ( $ascnum > 96 && $ascnum < 123){
+//         return $ascnum- 96; // Gives the score values based off the asc2 numbers for lowercase 1-26
+//     }
     
 
-}
+// }
 
 
 //sum all the scores together
