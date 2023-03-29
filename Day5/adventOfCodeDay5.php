d37882114577984ed2fa062c11b196d6f0c2cebd
Elijah Rallison <elikrallison@gmail.com>
2023-03-29 13:08:46 -0700

    finished day5 adding files for day6

--- Day5/adventOfCodeDay5.php
+++ Day5/adventOfCodeDay5.php
@@ -1,11 +1,60 @@
 <?php
-
 $input = file_get_contents('AdventDay5Example.txt');
+$inputArray = explode("\r\n\r\n", $input);
+$diagram = $inputArray[0];
+$rows = explode("\r\n", $diagram);
+$rows = array_reverse($rows);
+$moves = explode("\r\n", $inputArray[1]);
+$table = array();
+$rowNum = 1;
 
-[$stack, $procedure] = explode("\r\n\r\n", $input);
+foreach ($rows as $row) { 
 
-//print_r($stack);
-//print_r($procedure);
+    $colNum = 1;
+    
+    for ($i = 1; $i <= strlen($row); $i += 4) {
 
-$procedure = explode(" ", $procedure);
-print_r($procedure);
+        $char = substr($row, $i, 1);
+        $table[$colNum][$rowNum] = $char;
+        $colNum ++;
+    }
+    $rowNum ++;
+}
+$numCols = count($table);
+
+for ($i = 1; $i <= $numCols; $i ++) {
+
+    $col = $table[$i];
+    $numRows = count($col);
+
+    for ($j = 1; $j <= $numRows; $j ++) {
+
+        if ($col[$j] == " ") {
+
+            unset($table[$i][$j]);
+        }
+    }
+}
+
+foreach ($moves as $move) 
+{
+    $move = str_replace("move ", "", $move);
+    $move = str_replace(" from", "", $move);
+    $move = str_replace("to ", "", $move);
+    $moveArray = explode(" ", $move);  
+    $items = array();
+
+    for ($i = 0; $i < $moveArray[0]; $i ++) {
+        array_push($items, array_pop($table[$moveArray[1]]));
+    }
+
+    $items = array_reverse($items);
+    foreach ($items as $item) {
+        array_push($table[$moveArray[2]], $item);
+    }
+}
+
+foreach ($table as $col) {
+    echo array_pop($col);
+}
+?>
---
+++ Day6/AdventDay6.bat
@@ -0,0 +1,2 @@
+@echo ON
+cmd /k "C:\php\php.exe" adventofCodeDay6.php %*
