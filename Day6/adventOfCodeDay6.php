5177311657a2478c12736ec17c09066c49a64b42
Elijah Rallison <elikrallison@gmail.com>
2023-03-31 10:48:22 -0700

    freshest stuff

--- Day6/adventOfCodeDay6.php
+++ Day6/adventOfCodeDay6.php
@@ -1,1 +1,12 @@
 <?php
+
+// $example = 'mjqjpqmgbljsphdztnvjfqwrcgsmlb';
+
+// // $exampleArray = str_split($example, 1);
+// $exampleArray = str_split($example, strlen($example));
+// print_r($exampleArray);
+
+$example = 'mjqjpqmgbljsphdztnvjfqwrcgsmlb';
+$exampleArray = str_split($example);
+echo implode(' ', $exampleArray);
+echo $exampleArray[0];
