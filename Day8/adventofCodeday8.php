a2dddbbf37cade78f8297f205003391d01af91af
Elijah Rallison <elikrallison@gmail.com>
2023-08-27 16:24:34 -0700

    woah so gud

--- Day8/adventofCodeday8.php
+++ Day8/adventofCodeday8.php
@@ -1,62 +1,120 @@
 <?php
-$input = [
-    [3, 0, 3, 7, 3],
-    [2, 5, 5, 1, 2],
-    [6, 5, 3, 3, 2],
-    [3, 3, 5, 4, 9],
-    [3, 5, 3, 9, 0],
-];
 
-$visibleCount = 0;
-$height = count($input);
-$width = count($input[0]);
+$input = array_map(fn ($x) => str_split(trim($x), 1), file('adventday8input.txt'));
 
-for ($row = 0; $row < $height; $row++) {
-    for ($col = 0; $col < $width; $col++) {
-        $treeHeight = $input[$row][$col];
+function visible_col($row, $col, $input)
+{
+    $treeValue = $input[$row][$col];
+    $visible = true;
+    for ($i = $col - 1; $i >= 0; $i--) {
+        $visible = $visible && ($treeValue > $input[$row][$i]);
+    }
+    if ($visible) return true;
+    $visible = true;
+    for ($i = $col + 1; $i < count($input[$row]); $i++) {
+        $visible = $visible && ($treeValue > $input[$row][$i]);
+    }
+    return $visible;
+}
 
-        $visible = true;
+function visible_row($row, $col, $input)
+{
+    $treeValue = $input[$row][$col];
+    $visible = true;
+    for ($i = $row - 1; $i >= 0; $i--) {
+        $visible = $visible && ($treeValue > $input[$i][$col]);
+    }
+    if ($visible) return true;
+    $visible = true;
+    for ($i = $row + 1; $i < count($input); $i++) {
+        $visible = $visible && ($treeValue > $input[$i][$col]);
+    }
+    return $visible;
+}
 
-        for ($r = 0; $r < $row; $r++) {
-            if ($input[$r][$col] >= $treeHeight) {
-                $visible = false;
-                break;
-            }
+$visibleTrees = 0;
+
+for ($row = 0; $row < count($input); $row++) {
+    for ($col = 0; $col < count($input[$row]); $col++) {
+        if (visible_col($row, $col, $input) || visible_row($row, $col, $input)) {
+            $visibleTrees++;
         }
+    }
+}
+echo "Visible trees: $visibleTrees\n";
 
-        if ($visible) {
-            for ($r = $row + 1; $r < $height; $r++) {
-                if ($input[$r][$col] >= $treeHeight) {
-                    $visible = false;
-                    break;
-                }
-            }
-        }
+//part2
 
-        if ($visible) {
-            for ($c = 0; $c < $col; $c++) {
-                if ($input[$row][$c] >= $treeHeight) {
-                    $visible = false;
-                    break;
-                }
-            }
-        }
-
-        if ($visible) {
-            for ($c = $col + 1; $c < $width; $c++) {
-                if ($input[$row][$c] >= $treeHeight) {
-                    $visible = false;
-                    break;
-                }
-            }
-        }
-
-        if ($visible) {
-            $visibleCount++;
+function up($row, $col, $input)
+{
+    if ($row == 0) return 0;
+    $count = 1;
+    $savedValue = $input[$row][$col];
+    while (true) {
+        $row--;
+        if ($row <= 0) return $count;
+        if ($input[$row][$col] < $savedValue) {
+            $count++;
+        } else {
+            return $count;
         }
     }
 }
 
-echo "Number of visible trees: $visibleCount\n";
-?>
+function down($row, $col, $input)
+{
+    if ($row == count($input)-1) return 0;
+    $count = 1;
+    $savedValue = $input[$row][$col];
+    while (true) {
+        $row++;
+        if ($row >= count($input)-1) return $count;
+        if ($input[$row][$col] < $savedValue) {
+            $count++;
+        } else {
+            return $count;
+        }
+    }
+}
 
+function left($row, $col, $input)
+{
+    if ($col == 0) return 0;
+    $count = 1;
+    $savedValue = $input[$row][$col];
+    while (true) {
+        $col--;
+        if ($col <= 0) return $count;
+        if ($input[$row][$col] < $savedValue) {
+            $count++;
+        } else {
+            return $count;
+        }
+    }
+}
+
+function right($row, $col, $input)
+{
+    if ($col == count($input[$row])-1) return 0;
+    $count = 1;
+    $savedValue = $input[$row][$col];
+    while (true) {
+        $col++;
+        if ($col >= count($input[$row])-1) return $count;
+        if ($input[$row][$col] < $savedValue) {
+            $count++;
+        } else {
+            return $count;
+        }
+    }
+}
+
+$high = 0;
+for ($row = 0; $row < count($input); $row++) {
+    for ($col = 0; $col < count($input[$row]); $col++) {
+        $scenic = up($row, $col, $input) * down($row, $col, $input) *
+            left($row, $col, $input) * right($row, $col, $input);
+        if ($scenic > $high) $high = $scenic;
+    }
+}
+echo "Highest scenic score: $high\n";
