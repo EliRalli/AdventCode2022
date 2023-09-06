5c3a9d036827db323535fa51834b3902d282a098
Elijah Rallison <elikrallison@gmail.com>
2023-09-06 14:57:23 -0700

    weeeeeee

--- Day9/adventDay9.php
+++ Day9/adventDay9.php
@@ -5,10 +5,10 @@
 //head(H) and Tail(T) must always be touching (diagonal/ adjacent/overlapping)
 //puzzle gives us the input of the head movement
 //find how many positions the tail visited at least once
-$gridSize = 1000;
-$center = $grid/2;
-$grid = array_fill(0,$gridSize,array_fill(0, $gridSize, '.'));
-// Initialize grid dimensions and starting position
+
+$gridSize = 5000;
+$center = $gridSize / 2; // Center of the grid
+$grid = array_fill(0, $gridSize, array_fill(0, $gridSize, '.'));
 
 // Initial positions of head and tail
 $headX = $center;
@@ -19,38 +19,39 @@ $tailY = $center;
 // Mark the starting position as visited by tail
 $grid[$tailY][$tailX] = '#';
 
-// Example motion commands
-$motions = [
-    'R 4', 'U 4', 'L 3', 'D 1', 'R 4', 'D 1', 'L 5', 'R 2'
-];
+// Open the file for reading
+$file = fopen('adventDay9input.txt', 'r');
 
-foreach ($motions as $motion) {
-    list($direction, $steps) = explode(' ', $motion);
-    
-    // Update head position based on the motion command
-    switch ($direction) {
-        case 'R': $headX += $steps; break;
-        case 'L': $headX -= $steps; break;
-        case 'U': $headY -= $steps; break;
-        case 'D': $headY += $steps; break;
+if ($file) {
+    while (($line = fgets($file)) !== false) {
+        list($direction, $steps) = explode(' ', trim($line));
+        
+        switch ($direction) {
+            case 'R': $headX += $steps; break;
+            case 'L': $headX -= $steps; break;
+            case 'U': $headY -= $steps; break;
+            case 'D': $headY += $steps; break;
+        }
+        
+        if (abs($headX - $tailX) > 1 || abs($headY - $tailY) > 1) {
+            $tailX = $headX;
+            $tailY = $headY;
+        }
+        
+        $grid[$tailY][$tailX] = '#';
     }
     
-    // Update tail position if needed
-    if (abs($headX - $tailX) > 1 || abs($headY - $tailY) > 1) {
-        $tailX = $headX;
-        $tailY = $headY;
+    fclose($file);
+    
+    $visitedCount = 0;
+    foreach ($grid as $row) {
+        $visitedCount += count(array_filter($row, function($cell) {
+            return $cell === '#';
+        }));
     }
     
-    // Mark visited positions by tail
-    $grid[$tailY][$tailX] = '#';
+    echo "Positions visited at least once: $visitedCount\n";
+} else {
+    echo "Error opening the file.\n";
 }
 
-// Count the number of positions visited by tail
-$visitedCount = 0;
-foreach ($grid as $row) {
-    $visitedCount += count(array_filter($row, function($cell) {
-        return $cell === '#';
-    }));
-}
-
-echo "Positions visited at least once: $visitedCount\n";
